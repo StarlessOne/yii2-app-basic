0b2a1f32e666401cd53c66524ead424c29131547
starlessone <byankin.german@gmail.com>
2019-04-06 11:34:34 +0300

    Used gii to generate model and views/controller

--- models/Product.php
+++ models/Product.php
@@ -2,11 +2,48 @@
 
 namespace app\models;
 
-use yii\base\Model;
+use Yii;
 
-class Product extends Model {
-    public $id;
-    public $category;
-    public $name;
-    public $price;
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $price
+ * @property int $created_at
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'price', 'created_at'], 'required'],
+            [['created_at'], 'integer'],
+            [['name', 'price'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'price' => 'Price',
+            'created_at' => 'Created At',
+        ];
+    }
 }

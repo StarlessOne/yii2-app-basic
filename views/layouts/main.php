552171eb3235c09c51a595af9ad393c56bc6ae29
starlessone <byankin.german@gmail.com>
2019-04-25 16:12:52 +0300

    Changed layout view

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,10 +39,8 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'Test', 'url' => ['/test/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'My tasks', 'url' => ['/task/my']],
+            ['label' => 'New task', 'url' => ['/task/create']],
             Yii::$app->user->isGuest ? (
             ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
